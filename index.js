4a9c995a275ff1e7b9c80298c1a8e260aa438ee6
codax69 <128021116+codax69@users.noreply.github.com>
2024-07-08 22:02:12 +0530

    json

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-import express from "express";
+import express, { json } from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db.js";
 const app = express()
+app.use(express.static("public"))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.json({limit:"16kb"})); 
 dotenv.config({
   path: ".env"
 })
 const port = process.env.PORT
 
 app.get("/",(req,res)=>{
-  res.send("hello web app..!")
+  res.json({"data":"hello user"})
 })
 app.get("/health",(req,res)=>{
     res.send("web app perfectly working..!")
